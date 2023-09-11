f4d6200c1d6bf50e33e65498dec0916d14558a5e
kirakman <dourado178@gmail.com>
2023-09-11 20:15:34 -0300

    add router into registrar evento button

--- src/app/register-event/register-event.component.ts
+++ src/app/register-event/register-event.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register-event',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./register-event.component.css']
 })
 export class RegisterEventComponent {
+  constructor(private router: Router){}
 
+  navigateToDashboard(){
+    this.router.navigate(['dashboard']);
+  }
 }
